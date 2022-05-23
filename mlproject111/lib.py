9a5bebe463dab388e2301466fb81b20a188e5e2a
Pierre Jebara <pierrehenrijebara@gmail.com>
2022-05-23 14:39:00 +0200

    CI

---
+++ mlproject111/lib.py
@@ -0,0 +1,12 @@
+def even(my_list):
+    list1=[]
+    for x in my_list:
+        if x % 2==0:
+            list1.append(x)
+    return list1
+def odd(my_list):
+    list2=[]
+    for x in my_list:
+        if x %3==0:
+            list2.append(x)
+    return list2
